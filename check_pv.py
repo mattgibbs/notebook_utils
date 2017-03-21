37dc91e9f6771dcdf2afc7f7b1e997a07268d874
physics group account <physics@opi20.slac.stanford.edu>
2017-03-21 11:29:34 -0700

    Add a Checklist object for maintaining a list of checks.  Also fixes several bugs.

--- check_pv.py
+++ check_pv.py
@@ -1,45 +1,152 @@
 import epics
 import time
-def is_within_limits(pv, low_limit, high_limit):
+from ipywidgets import widgets, Layout
+from IPython.display import display, HTML
+
+class Checklist(object):
+	def __init__(self):
+		self._checklist = []
+		self.result_template = "<span style='color: {color}; font-weight: bold;'>{msg}</span>"
+
+	def to_html(self):
+		table = "<table>\n\t<tr><th>Description</th><th>Condition</th><th>Recorded Value</th><th>Result</th></tr>\n"
+		for row in self._checklist:
+			row = "\t<tr><td>{desc}</td><td>{cond}</td><td>{val}</td><td>{res}</td></tr>\n".format(desc=row['description'], cond=row['condition'], val=row['val'], res=row['result'])
+			table += row
+		table += "</table>"
+		return table
+
+	def display_results(self):
+		display(HTML(self.to_html()))
+
+	def append(self, new_item):
+		self._checklist.append(new_item)
+
+	def append_custom_item(self, description, condition, value, result):
+		self._checklist.append({"description": description, "condition": condition, "val": value, "result": result})
+
+	def is_within_limits(self, pv, low_limit, high_limit, description=None):
+		return is_within_limits(pv, low_limit, high_limit, description, self)
+
+	def is_below_limit(self, pv, high_limit, description=None):
+		return is_below_limit(pv, high_limit, description, self)
+
+	def is_above_limit(self, pv, low_limit, description=None):
+		return is_above_limit(pv, low_limit, description, self)
+
+	def is_updating(self, pv, timeout=10.0, description=None):
+		return is_updating(pv, timeout, description, self)
+
+	def has_value(self, pv, val, tolerance=None, description=None):
+		return has_value(pv, val, tolerance, description, self)
+
+	def custom_with_checkbox(self, description, condition):
+		item_index = len(self._checklist)
+		def toggle_cb(change):
+			val = change['new']
+			msg = "Not OK"
+			if val == True:
+				msg = "OK"
+			self._checklist[item_index]['result'] = msg
+		self.append_custom_item(description, condition, "", "Not OK")
+		l = widgets.Label(description, layout=Layout(width='75%'))
+		cb = widgets.Checkbox(value=False, description="OK?", layout=Layout(width='25%'))
+		cb.observe(toggle_cb, names='value')
+		item = widgets.HBox([l, cb])
+		display(item)
+
+def is_within_limits(pv, low_limit, high_limit, description=None, checklist=None):
 	val = epics.caget(pv)
 	ok = False
-	if low_limit is not None and high_limit is not None:
+	msg = None
+	if val is None:
+		ok = False
+		msg = "Error: Could not connect to PV"
+	elif low_limit is not None and high_limit is not None:
 		ok = low_limit < val < high_limit
+		if ok:
+			msg = "OK"
+		elif val < low_limit:
+			msg = "Too Low"
+		elif val > high_limit:
+			msg = "Too High"
 	elif low_limit is None and high_limit is not None:
 		ok = val < high_limit
+		if ok:
+			msg = "OK"
+		else:
+			msg = "Too High"
 	elif low_limit is not None and high_limit is None:
 		ok = val > low_limit
+		if ok:
+			msg = "OK"
+		else:
+			msg = "Too Low"
 	else:
 		raise ValueError("Both high and low limit cannot be None.")
+	if checklist is not None:
+		if description is None:
+			description = pv
+		colors = {True: "green", False: "red"}
+		result = checklist.result_template.format(color=colors[ok], msg=msg)
+		condition = ""
+		if low_limit is not None:
+			condition += "{} < ".format(low_limit)
+		condition += "Recorded Value"
+		if high_limit is not None:
+			condition += " < {}".format(high_limit)
+		checklist.append({'description': description, 'condition': condition, 'val': val, 'result': result})
 	return (val, ok)
 
-def is_below_limit(pv, high_limit):
-	return is_within_limits(pv, low_limit=None, high_limit=high_limit)
+def is_below_limit(pv, high_limit, description=None, checklist=None):
+	return is_within_limits(pv, low_limit=None, high_limit=high_limit, description=description, checklist=checklist)
 
-def is_above_limit(pv, low_limit):
-	return is_within_limits(pv, low_limit=low_limit, high_limit=None)
+def is_above_limit(pv, low_limit, description=None, checklist=None):
+	return is_within_limits(pv, low_limit=low_limit, high_limit=None, description=description, checklist=checklist)
 
-def is_updating(pv, timeout=10.0):
+def is_updating(pv, timeout=10.0, description=None, checklist=None):
 	p = epics.PV(pv)
 	try:
-		connected = p.wait_for_conection()
+		connected = p.wait_for_conection(timeout=timeout)
 		if not connected:
 			raise Exception("Could not connect to PV {}".format(pv))
 	except AttributeError:
 		pass
-	first_val = p.get()
+	first_val = p.get(timeout=timeout)
+	if first_val is None:
+		raise Exception("Could not connect to PV {}".format(pv))
 	elapsed_time = 0.0
 	wait_time = 0.1
+	ok = False
 	while elapsed_time < timeout:
-		second_val = p.get()
+		second_val = p.get(timeout=timeout)
+		if second_val is None:
+			raise Exception("Unexpected disconnection from PV {}".format(pv))
 		if first_val != second_val:
-			return True
+			ok = True
+			break
 		time.sleep(wait_time)
 		elapsed_time += wait_time
-	return False
+	if checklist is not None:
+		if description is None:
+			description = pv
+		colors = {True: "green", False: "red"}
+		msg = {True: "PV is updating", False: "PV not updating"}
+		condition = "is updating"
+		result = checklist.result_template.format(color=colors[ok], msg=msg[ok])
+		checklist.append({"description": description, "condition": condition, "val": second_val, "result": result})
+	return ok
 
-def has_value(pv, val, tolerance=None):
-	if tolerance:
-		return is_within_limits(pv, low_limit=val-tolerance, high_limit=val+tolerance)[1]
-	else:
-		return epics.caget(pv) == val
+def has_value(pv, val, tolerance=None, description=None, checklist=None):
+	if tolerance is not None:
+		return is_within_limits(pv, low_limit=val-tolerance, high_limit=val+tolerance, description=description, checklist=checklist)[1]
+	ok = epics.caget(pv) == val
+	if checklist is not None:
+		if description is None:
+			description = pv
+		colors = {True: "green", False: "red"}
+		msg = {True: "Equal", False: "Not Equal"}
+		condition = "Recorded Value == {}".format(val)
+		result = checklist.result_template.format(color=colors[ok], msg=msg[ok])
+		checklist.append({"description": description, "condition": condition, "val": val, "result": result})
+	return ok
