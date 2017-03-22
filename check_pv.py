1a91a6e6ad806f87ec29b0334333865af92bdc54
physics group account <physics@opi20.slac.stanford.edu>
2017-03-21 19:04:05 -0700

    Checklist items now print a message rather than returning the value and boolean status of the check.

--- check_pv.py
+++ check_pv.py
@@ -26,19 +26,58 @@ class Checklist(object):
 		self._checklist.append({"description": description, "condition": condition, "val": value, "result": result})
 
 	def is_within_limits(self, pv, low_limit, high_limit, description=None):
-		return is_within_limits(pv, low_limit, high_limit, description, self)
+		val, ok = is_within_limits(pv, low_limit, high_limit, description, self)
+		msg = None
+		if ok:
+			msg = "Value of {pv} ({val}) is above low limit ({low_limit}) and below high limit ({high_limit}): Test Passed".format(pv=pv, val=val, low_limit=low_limit, high_limit=high_limit)
+		else:
+			if val < low_limit:
+				msg = "Value of {pv} ({val}) is below low limit ({low_limit}): Test Failed".format(pv=pv, val=val, low_limit=low_limit)
+			elif val > high_limit:
+				msg = "Value of {pv} ({val}) is above high limit ({high_limit}): Test Failed".format(pv=pv, val=val, high_limit=high_limit)
+		print(msg)
 
 	def is_below_limit(self, pv, high_limit, description=None):
-		return is_below_limit(pv, high_limit, description, self)
+		val, ok = is_below_limit(pv, high_limit, description, self)
+		msg = None
+		if ok:
+			msg = "Value of {pv} ({val}) is below high limit ({high_limit}): Test Passed".format(pv=pv, val=val, high_limit=high_limit)
+		else:
+			msg = "Value of {pv} ({val}) is above high limit ({high_limit}): Test Failed".format(pv=pv, val=val, high_limit=high_limit)
+		print(msg)
 
 	def is_above_limit(self, pv, low_limit, description=None):
-		return is_above_limit(pv, low_limit, description, self)
+		val, ok = is_above_limit(pv, low_limit, description, self)
+		msg = None
+		if ok:
+			msg = "Value of {pv} ({val}) is above low limit ({low_limit}): Test Passed".format(pv=pv, val=val, low_limit=low_limit)
+		else:
+			msg = "Value of {pv} ({val}) is below low limit ({low_limit}): Test Failed".format(pv=pv, val=val, low_limit=low_limit)
+		print(msg)
 
 	def is_updating(self, pv, timeout=10.0, description=None):
-		return is_updating(pv, timeout, description, self)
+		ok = is_updating(pv, timeout, description, self)
+		msg = None
+		if ok:
+			msg = "{pv} updated within {timeout} seconds: Test Passed".format(pv=pv, timeout=timeout)
+		else:
+			msg = "{pv} did not update within {timeout} seconds: Test Failed".format(pv=pv, timeout=timeout)
+		print(msg)
 
 	def has_value(self, pv, val, tolerance=None, description=None):
-		return has_value(pv, val, tolerance, description, self)
+		recorded_val, ok = has_value(pv, val, tolerance, description, self)
+		msg = None
+		if ok:
+			msg = "Value of {pv} ({recorded_val}) is equal to {val}".format(pv=pv, recorded_val=recorded_val, val=val)
+			if tolerance is not None:
+				msg += " +/- {tol}".format(tol=tolerance)
+			msg += ": Test Passed"
+		else:
+			msg = "Value of {pv} ({recorded_val}) is not equal to {val}".format(pv=pv, recorded_val=recorded_val, val=val)
+			if tolerance is not None:
+				msg += " +/- {tol}".format(tol=tolerance)
+			msg += ": Test Failed"
+		print(msg)
 
 	def custom_with_checkbox(self, description, condition):
 		item_index = len(self._checklist)
@@ -65,7 +104,7 @@ def is_within_limits(pv, low_limit, high_limit, description=None, checklist=None
 	elif low_limit is not None and high_limit is not None:
 		ok = low_limit < val < high_limit
 		if ok:
-			msg = "OK"
+			msg = "Within Limits"
 		elif val < low_limit:
 			msg = "Too Low"
 		elif val > high_limit:
@@ -73,13 +112,13 @@ def is_within_limits(pv, low_limit, high_limit, description=None, checklist=None
 	elif low_limit is None and high_limit is not None:
 		ok = val < high_limit
 		if ok:
-			msg = "OK"
+			msg = "Within Limits"
 		else:
 			msg = "Too High"
 	elif low_limit is not None and high_limit is None:
 		ok = val > low_limit
 		if ok:
-			msg = "OK"
+			msg = "Within Limits"
 		else:
 			msg = "Too Low"
 	else:
@@ -131,7 +170,7 @@ def is_updating(pv, timeout=10.0, description=None, checklist=None):
 		if description is None:
 			description = pv
 		colors = {True: "green", False: "red"}
-		msg = {True: "PV is updating", False: "PV not updating"}
+		msg = {True: "PV is updating", False: "PV is not updating"}
 		condition = "is updating"
 		result = checklist.result_template.format(color=colors[ok], msg=msg[ok])
 		checklist.append({"description": description, "condition": condition, "val": second_val, "result": result})
@@ -139,8 +178,9 @@ def is_updating(pv, timeout=10.0, description=None, checklist=None):
 
 def has_value(pv, val, tolerance=None, description=None, checklist=None):
 	if tolerance is not None:
-		return is_within_limits(pv, low_limit=val-tolerance, high_limit=val+tolerance, description=description, checklist=checklist)[1]
-	ok = epics.caget(pv) == val
+		return is_within_limits(pv, low_limit=val-tolerance, high_limit=val+tolerance, description=description, checklist=checklist)
+	readback = epics.caget(pv)
+	ok = readback == val
 	if checklist is not None:
 		if description is None:
 			description = pv
@@ -148,5 +188,5 @@ def has_value(pv, val, tolerance=None, description=None, checklist=None):
 		msg = {True: "Equal", False: "Not Equal"}
 		condition = "Recorded Value == {}".format(val)
 		result = checklist.result_template.format(color=colors[ok], msg=msg[ok])
-		checklist.append({"description": description, "condition": condition, "val": val, "result": result})
-	return ok
+		checklist.append({"description": description, "condition": condition, "val": readback, "result": result})
+	return (readback, ok)
